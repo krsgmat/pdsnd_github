44c0e9731c2d33656d84f23422ebfb5c004dcd7f
Matthew Gray Kirsten <krsgmat@gmail.com>
2020-11-14 18:38:25 +0200

    Update comments in time stats function

--- bikeshare.py
+++ bikeshare.py
@@ -53,7 +53,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the selected city and filters by month and day if applicable.
 
     Args:
         (str) city - name of the city to analyze
